d403c34405d6033794f3cbebf7816eae8e38db5d
Bhanu <uabhanu@gmail.com>
2017-05-29 13:05:17 +0100

    Camera Movement Logic WIP

---
+++ CV Gamified/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+	
+	void Update()
+    {
+        //transform.LookAt(playerTransform.position);
+        transform.position = new Vector3(playerTransform.position.x , playerTransform.position.y + 1.48f , playerTransform.position.z - 4f);
+	}
+}
