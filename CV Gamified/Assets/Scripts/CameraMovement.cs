333378f91ab3d85850f9ba309d4786c96145a8f6
Bhanu <uabhanu@gmail.com>
2017-05-29 18:18:30 +0100

    Camera Movement WIP

--- CV Gamified/Assets/Scripts/CameraMovement.cs
+++ CV Gamified/Assets/Scripts/CameraMovement.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    Camera m_camera;
-    float m_currentX = 0f , m_currentY = 0f , m_distance = 10f , m_sensitivityX = 4f , m_sensitivityY = 1f;
-    [SerializeField] float m_xAngleMax , m_xAngleMin , m_yAngleMax, m_yAngleMin;
-    [SerializeField] Transform m_cameraTransform , m_lookAt;
-
-    private void Start()
-    {
-        m_cameraTransform = transform;
-        m_camera = Camera.main;
-    }
+    float m_currentX = 0f , m_currentY = 0f;
+    [SerializeField] float m_xAngleMax , m_xAngleMin , m_yAngleMax , m_yAngleMin , m_distance;
+    [SerializeField] Transform m_playerTransform;
 
     private void Update()
     {
@@ -28,7 +21,7 @@ public class CameraMovement : MonoBehaviour
     {
         Vector3 dir = new Vector3(0f , 0f , -m_distance);
         Quaternion rotation = Quaternion.Euler(m_currentY , m_currentX , 0f);
-        m_cameraTransform.position = m_lookAt.position + rotation * dir;
-        m_cameraTransform.LookAt(m_lookAt.position);
+        transform.position = m_playerTransform.position + rotation * dir;
+        transform.LookAt(m_playerTransform.position);
     }
 }
