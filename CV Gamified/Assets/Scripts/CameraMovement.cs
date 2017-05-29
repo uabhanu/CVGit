e7cbe17df8931ec0160b579b58cf9b2c2e6de8a2
Bhanu <uabhanu@gmail.com>
2017-05-29 17:40:16 +0100

    Camera Movement Logic almost done

--- CV Gamified/Assets/Scripts/CameraMovement.cs
+++ CV Gamified/Assets/Scripts/CameraMovement.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] Transform playerTransform;
-	
-	void Update()
+    Camera m_camera;
+    float m_currentX = 0f , m_currentY = 0f , m_distance = 10f , m_sensitivityX = 4f , m_sensitivityY = 1f;
+    [SerializeField] float m_xAngleMax , m_xAngleMin , m_yAngleMax, m_yAngleMin;
+    [SerializeField] Transform m_cameraTransform , m_lookAt;
+
+    private void Start()
     {
-        //transform.LookAt(playerTransform.position);
-        transform.position = new Vector3(playerTransform.position.x , playerTransform.position.y + 1.48f , playerTransform.position.z - 4f);
-	}
+        m_cameraTransform = transform;
+        m_camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        m_currentX += Input.GetAxis("Mouse X");
+        m_currentY += Input.GetAxis("Mouse Y");
+
+        m_currentX = Mathf.Clamp(m_currentX , m_xAngleMin , m_xAngleMax);
+        m_currentY = Mathf.Clamp(m_currentY , m_yAngleMin , m_yAngleMax);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0f , 0f , -m_distance);
+        Quaternion rotation = Quaternion.Euler(m_currentY , m_currentX , 0f);
+        m_cameraTransform.position = m_lookAt.position + rotation * dir;
+        m_cameraTransform.LookAt(m_lookAt.position);
+    }
 }
